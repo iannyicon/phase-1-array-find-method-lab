a09eb436805ddb1839b5fed74871742ba446be60
Iann <iconian4@gmail.com>
2025-03-10 22:42:33 +0300

    initiate commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+// code your solution 
+function superbowlWin(array) {
+    let win = array.find(function(e){
+        return e.result === "W";
+    });
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
